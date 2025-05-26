4dd43d91eaa503bbdb1c71aec6aa40cc97323d31
aashishkr006 <aashishkr006@gmail.com>
2025-05-26 18:54:25 +0530

    Structure change of display section and the buttons, form tag added

---
+++ Calculator/script.js
@@ -0,0 +1,7 @@
+let input = document.getElementById('operations');
+const output = document.getElementById('result');
+const keys = document.getElementById('keys');
+
+// const buttons = document.querySelectorAll('keys')
+
+
